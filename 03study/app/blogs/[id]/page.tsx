9de1313e1cf877dd3f3f6fbdbc000b131ee2e92f
tsunat973 <t23am018@yamanashi.ac.jp>
2026-08-26 20:35:11 +0900

    SSG

--- 03study/app/blogs/[id]/page.tsx
+++ 03study/app/blogs/[id]/page.tsx
@@ -1,16 +1,20 @@
 
+import { Suspense } from "react";
+
 async function BlogContent({ params }: { params: Promise<{ id: string }> }) {
-
-
-    const { id } = await params;
-    return (
-        <div>
-            <h1>BlogDetailページ</h1>
-            <p>ID: {id}</p>
-        </div>
-    );
+  const { id } = await params;
+  return (
+    <div>
+      <h1>BlogDetailページ</h1>
+      <p>ID: {id}</p>
+    </div>
+  );
 }
 
-export default function BlogDetail({ params}: { params: Promise<{ id: string }> }) {
-    return <BlogContent params={params} />;
+export default function BlogDetail({ params }: { params: Promise<{ id: string }> }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BlogContent params={params} />
+    </Suspense>
+  );
 }
--- 03study/app/blogs/page.tsx
+++ 03study/app/blogs/page.tsx
@@ -1,7 +1,44 @@
-export default function Blogs() {
+"use cache"
+
+import { MicrocmsResponse } from "@/domain/Article"
+import axios from "axios"
+import Image from "next/image";
+
+
+
+const Blogs = async () => {
+  const getBlogs = async () => {
+    const response = await axios.get<MicrocmsResponse>(
+      "https://gsnz1d1ua1.microcms.io/api/v1/blogs",
+      {
+        headers: {
+          "X-MICROCMS-API-KEY": process.env.MICROCMS_API_KEY,
+        },
+      }
+    );
+
+    return response.data.contents.map((item) => ({
+      id: item.id,
+      title: item.title,
+      image: item.eyecatch.url,
+      url: `/blogs/${item.id}`,
+    }));
+  };
+
+  const blogs = await getBlogs();
   return (
     <div>
-      <h1>Blogsページ</h1>
+      <h1>ブログ一覧</h1>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+             <Image src={blog.image} width={100} height={100} alt="" />
+             <a href={blog.url}>{blog.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+export default Blogs;
